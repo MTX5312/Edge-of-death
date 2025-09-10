a60560ddcaf0cdc243d87a4c6c0323391636876a
Tomás Méndez <tomasmendez@outlook.com>
2025-09-10 19:44:53 -0300

    Codigo de movimiento

--- Assets/Scenes/ScriptJugador.cs
+++ Assets/Scenes/ScriptJugador.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ScriptJugador : MonoBehaviour
 {
+    private float speed = 15f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+//        if (speed > 30)
+//        {
+//            speed + 1;
+//        }
+
+        transform.Translate(new Vector3(x, 0, y) * Time.deltaTime * speed);
     }
 }
