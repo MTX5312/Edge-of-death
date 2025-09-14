c12b488d12b11caebcd38e1e759096e54873517a
Tomás Méndez <tomasmendez@outlook.com>
2025-09-14 19:26:07 -0300

    Deslizar
    - Nueva funcion de deslizar
    - Se acomodo el comportamiento para caminar en una funcion.
    - Se crearon variables utilizadas especificamente en cierta funcion y variables globales, como velocidad y direccion

--- Assets/Scenes/ScriptJugador.cs
+++ Assets/Scenes/ScriptJugador.cs
@@ -1,20 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
 
 public class ScriptJugador : MonoBehaviour
 {
-    public float aceleracion = 5f;
-    public float velocidadMaxima = 100f;
-    public float velocidadMinima = 20f;
     public float velocidadActual = 20f;
-    public float desaceleracion = 5f;
-    public float freno = 10f;
-
-    public ScriptCamara camara;
-    public float sensibilidad = 6f;
+    public float tiempoRestanteSlide = 0f;
+    private Vector3 direccion;
+    bool deslizando = false;
 
     public Transform Body;
+    public ScriptCamara camara;
 
     private void Start()
     {
@@ -27,25 +25,15 @@ public class ScriptJugador : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        if (Input.GetKey(KeyCode.W))
-        {
-            velocidadActual += aceleracion * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            velocidadActual -= freno * Time.deltaTime;
-        }
-        else
-        {
-            velocidadActual -= desaceleracion * Time.deltaTime;
-        }
-        velocidadActual = Mathf.Clamp(velocidadActual, velocidadMinima, velocidadMaxima);
-
+        Caminar();
+        Deslizar();
+        
         Vector3 movimiento = new Vector3(x, 0, y).normalized;
-
+        
+        //Acomodar camara
         if (movimiento.sqrMagnitude > 0.01f)
         {
-            Vector3 direccion = Body.TransformDirection(movimiento);
+            direccion = Body.TransformDirection(movimiento);
 
             transform.Translate(direccion * velocidadActual * Time.deltaTime, Space.World);
 
@@ -55,8 +43,66 @@ public class ScriptJugador : MonoBehaviour
                 transform.rotation = Quaternion.Slerp(
                     transform.rotation,
                     rotacionObjetivo,
-                    sensibilidad * Time.deltaTime
+                    6f * Time.deltaTime
                 );
+            }
+        }
+    }
+
+    void Caminar()
+    {
+        float aceleracion = 5f;
+        float velocidadMaxima = 100f;
+        float velocidadMinima = 20f;
+        float desaceleracion = 5f;
+        float freno = 20f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (velocidadActual < velocidadMaxima)
+            {
+                velocidadActual += aceleracion * Time.deltaTime;
+            }
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (velocidadActual > velocidadMinima)
+            {
+                velocidadActual -= freno * Time.deltaTime;
+            }    
+        }
+        else
+        {
+            if (velocidadActual > velocidadMinima)
+            {
+                velocidadActual -= desaceleracion * Time.deltaTime;
+            }
+        }
+    }
+
+    void Deslizar()
+    {
+        float duracionSlide = 2f;
+        float velocidadExtra = 20f;
+        float cooldownSlide = 1.5f;
+        float tiempoUltimoSlide = -10;
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !deslizando && Time.time > tiempoUltimoSlide + cooldownSlide)
+        {
+            deslizando = true;
+            velocidadActual += velocidadExtra;
+            direccion = transform.forward;
+            tiempoRestanteSlide = duracionSlide;
+            tiempoUltimoSlide = Time.time;
+        }
+        if (deslizando)
+        {
+            tiempoRestanteSlide -= Time.deltaTime;
+            transform.Translate(direccion * velocidadActual * Time.deltaTime, Space.World);
+            velocidadActual = Mathf.MoveTowards(velocidadActual, 20f, (velocidadExtra / duracionSlide) * Time.deltaTime);
+
+            if (tiempoRestanteSlide <= 0f)
+            {
+                deslizando = false;
             }
         }
     }
