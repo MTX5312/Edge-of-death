a4fe8c6240ccc741982f040ef7b4fc802566abb8
Tomás Méndez <tomasmendez@outlook.com>
2025-09-27 22:25:06 -0300

    Respawn con penalizacion

--- Assets/Scenes/DeathZoneScript.cs
+++ Assets/Scenes/DeathZoneScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DeathZoneScript : MonoBehaviour
 {
     public Transform respawnPoint;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -16,6 +17,12 @@ public class DeathZoneScript : MonoBehaviour
             {
                 rb.velocity = Vector3.zero;
                 rb.angularVelocity = Vector3.zero;
+            }
+
+            ScriptJugador playerScript = other.GetComponent<ScriptJugador>();
+            if (playerScript != null)
+            {
+                playerScript.velocidadActual = 10f;
             }
         }
     }
