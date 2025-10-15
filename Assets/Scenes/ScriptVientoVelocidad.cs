5a24f1969e2ffdf65b01e0eebc5c239890c46843
Gabriel <gm888641@gmail.com>
2025-10-15 16:58:01 -0300

    Añadido código al ScriptVientoVelocidad.

--- Assets/Scenes/ScriptVientoVelocidad.cs
+++ Assets/Scenes/ScriptVientoVelocidad.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ScriptVientoVelocidad : MonoBehaviour
+public class VientoVelocidad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody jugadorRB;       // Rigidbody del jugador
+    public ParticleSystem viento50;   // Efecto de viento cuando pasa 50
+    public ParticleSystem viento100;  // Efecto de viento cuando pasa 100
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Calculamos la velocidad actual (sin dirección)
+        float velocidadActual = jugadorRB.velocity.magnitude;
+
+        // Si está entre 50 y 99 → activar viento50
+        if (velocidadActual >= 50f && velocidadActual < 100f)
+        {
+            if (!viento50.isPlaying)
+                viento50.Play();
+            if (viento100.isPlaying)
+                viento100.Stop();
+        }
+        // Si alcanza o supera 100 → activar viento100
+        else if (velocidadActual >= 100f)
+        {
+            if (!viento100.isPlaying)
+                viento100.Play();
+            if (viento50.isPlaying)
+                viento50.Stop();
+        }
+        // Si baja de 50 → apagar ambos
+        else
+        {
+            if (viento50.isPlaying)
+                viento50.Stop();
+            if (viento100.isPlaying)
+                viento100.Stop();
+        }
     }
 }
+
