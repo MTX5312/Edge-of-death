bb5a6279fbc8b778f1ac4ba2c4c8f3e1458669e8
Tomás Méndez <tomasmendez@outlook.com>
2025-10-27 17:11:19 -0300

    Se añadio el codigo para activar y desactivar los efectos por zona

--- Assets/Scenes/#Efectos/ScripEfectoZonas.cs
+++ Assets/Scenes/#Efectos/ScripEfectoZonas.cs
@@ -1,9 +1,9 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScripEfectoZonas : MonoBehaviour
 {
+    [Header("Efectos de Zona")]
     public Image traicionEffect;
     public Image gulaEffect;
     public Image avariciaEffect;
@@ -14,25 +14,53 @@ public class ScripEfectoZonas : MonoBehaviour
 
     void Start()
     {
-        if (traicionEffect != null)
-            traicionEffect.gameObject.SetActive(false);
-        if (gulaEffect != null)
-            gulaEffect.gameObject.SetActive(false);
-        if (avariciaEffect != null)
-            avariciaEffect.gameObject.SetActive(false);
-        if (violenciaEffect != null)
-            violenciaEffect.gameObject.SetActive(false);
-        if (lujuriaEffect != null)
-            lujuriaEffect.gameObject.SetActive(false);
-        if (herejiaEffect != null)
-            herejiaEffect.gameObject.SetActive(false);
-        if (perezaEffect != null)
-            perezaEffect.gameObject.SetActive(false);
+        OcultarTodosLosEfectos();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MostrarEfecto(string nombreZona)
     {
-        
+        OcultarTodosLosEfectos();
+
+        switch (nombreZona)
+        {
+            case "Traicion":
+                Activar(traicionEffect);
+                break;
+            case "Gula":
+                Activar(gulaEffect);
+                break;
+            case "Avaricia":
+                Activar(avariciaEffect);
+                break;
+            case "Violencia":
+                Activar(violenciaEffect);
+                break;
+            case "Lujuria":
+                Activar(lujuriaEffect);
+                break;
+            case "Herejia":
+                Activar(herejiaEffect);
+                break;
+            case "Pereza":
+                Activar(perezaEffect);
+                break;
+        }
+    }
+
+    public void OcultarTodosLosEfectos()
+    {
+        if (traicionEffect) traicionEffect.gameObject.SetActive(false);
+        if (gulaEffect) gulaEffect.gameObject.SetActive(false);
+        if (avariciaEffect) avariciaEffect.gameObject.SetActive(false);
+        if (violenciaEffect) violenciaEffect.gameObject.SetActive(false);
+        if (lujuriaEffect) lujuriaEffect.gameObject.SetActive(false);
+        if (herejiaEffect) herejiaEffect.gameObject.SetActive(false);
+        if (perezaEffect) perezaEffect.gameObject.SetActive(false);
+    }
+
+    private void Activar(Image img)
+    {
+        if (img != null)
+            img.gameObject.SetActive(true);
     }
 }
