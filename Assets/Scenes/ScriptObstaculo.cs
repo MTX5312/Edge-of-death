7b084ff9c1410842b938f71220ab950e48ac509b
Tomás Méndez <tomasmendez@outlook.com>
2025-10-29 17:03:21 -0300

    Revert "Nueva collision triggered para el obstaculo"
    This reverts commit c6d5db952b562c38f9ca0b48170fbd99eb27283b.

--- Assets/Scenes/ScriptObstaculo.cs
+++ Assets/Scenes/ScriptObstaculo.cs
@@ -1,15 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ScriptObstaculo : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        // Verifica si el objeto que entró al trigger tiene el ScriptJugador
-        ScriptJugador jugador = other.GetComponent<ScriptJugador>();
-
+        //Esto chequea si el objeto que chocó tiene el ScriptJugador
+        ScriptJugador jugador = collision.gameObject.GetComponent<ScriptJugador>();
         if (jugador != null)
+
         {
+            //Penaliza y reduce la velocidad a 20
             jugador.velocidadActual = 20f;
+
             Debug.Log("Jugador penalizado: velocidad reducida a 20");
         }
     }
