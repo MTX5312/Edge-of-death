5bd309da08a13357101261a4a55accfe1e5d783d
Tomás Méndez <tomasmendez@outlook.com>
2025-11-20 16:44:32 -0300

    Estuctura de nivel 4 completada y correciones

--- Assets/Scenes/#Efectos/ZonaDetector.cs
+++ Assets/Scenes/#Efectos/ZonaDetector.cs
@@ -2,25 +2,57 @@
 
 public class ZonaDetector : MonoBehaviour
 {
+    public ScripEfectoZonas efectoHUD;
     public SlowMoOnDash slowMo;
+
     private string zonaActual = "";
+
+    private void Start()
+    {
+        if (efectoHUD != null)
+            efectoHUD.MostrarEfecto("");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Pereza")
+        if (efectoHUD == null) return;
+
+        switch (other.gameObject.name)
         {
-            slowMo.estaEnPereza = true;
+            case "Traicion":
+            case "Gula":
+            case "Avaricia":
+            case "Violencia":
+            case "Lujuria":
+            case "Herejia":
+            case "Pereza":
+                zonaActual = other.gameObject.name;
+                efectoHUD.MostrarEfecto(zonaActual);
+
+                if (other.gameObject.name == "Pereza")
+                {
+                    slowMo.estaEnPereza = true;
+                }
+                break;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.name == "Pereza")
-        {
-            slowMo.estaEnPereza = false;
+        if (efectoHUD == null) return;
 
-            Time.timeScale = 1f;
-            Time.fixedDeltaTime = 0.02f;
+        if (other.gameObject.name == zonaActual)
+        {
+            zonaActual = "";
+            efectoHUD.MostrarEfecto("");
+
+            if (other.gameObject.name == "Pereza")
+            {
+                slowMo.estaEnPereza = false;
+
+                Time.timeScale = 1f;
+                Time.fixedDeltaTime = 0.02f;
+            }
         }
     }
 }
