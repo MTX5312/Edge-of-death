44324d17d24d7395918232bbad6385fcddd78f98
Tomás Méndez <tomasmendez@outlook.com>
2025-11-21 19:24:12 -0300

    Nuevas carpetas y codigos de skybox

--- Assets/Scenes/#Efectos/ZonaDetector.cs
+++ Assets/Scenes/#Efectos/ZonaDetector.cs
@@ -5,12 +5,16 @@ public class ZonaDetector : MonoBehaviour
     public ScripEfectoZonas efectoHUD;
     public SlowMoOnDash slowMo;
 
+    public SkyboxManager skyboxManager;
+
     private string zonaActual = "";
 
     private void Start()
     {
         if (efectoHUD != null)
             efectoHUD.MostrarEfecto("");
+
+        skyboxManager?.CambiarSkybox("");
     }
 
     private void OnTriggerEnter(Collider other)
@@ -26,30 +30,31 @@ public class ZonaDetector : MonoBehaviour
             case "Lujuria":
             case "Herejia":
             case "Pereza":
+
                 zonaActual = other.gameObject.name;
                 efectoHUD.MostrarEfecto(zonaActual);
 
+                skyboxManager?.CambiarSkybox(zonaActual);
+
                 if (other.gameObject.name == "Pereza")
-                {
                     slowMo.estaEnPereza = true;
-                }
+
                 break;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (efectoHUD == null) return;
-
         if (other.gameObject.name == zonaActual)
         {
             zonaActual = "";
             efectoHUD.MostrarEfecto("");
 
+            skyboxManager?.CambiarSkybox("");
+
             if (other.gameObject.name == "Pereza")
             {
                 slowMo.estaEnPereza = false;
-
                 Time.timeScale = 1f;
                 Time.fixedDeltaTime = 0.02f;
             }
