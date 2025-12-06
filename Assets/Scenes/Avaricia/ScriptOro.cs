e6aed78472e79d349b9dc394c977bf55f30ed63f
Tomás Méndez <tomasmendez@outlook.com>
2025-12-06 11:45:01 -0300

    Cambios en objeto Oro
    Nuevo modelo 3D y comportamiento

--- Assets/Scenes/Avaricia/ScriptOro.cs
+++ Assets/Scenes/Avaricia/ScriptOro.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class ScriptOro : MonoBehaviour
 {
+    public float velocidadRotacion = 180f;
+
+    private Vector3 ejeRotacion;
+
+    void Start()
+    {
+        int eje = Random.Range(0, 3);
+
+        if (eje == 0)
+            ejeRotacion = Vector3.right;
+        else if (eje == 1)
+            ejeRotacion = Vector3.up;
+        else
+            ejeRotacion = Vector3.forward;
+    }
+
+    void Update()
+    {
+        transform.Rotate(ejeRotacion * velocidadRotacion * Time.deltaTime);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
